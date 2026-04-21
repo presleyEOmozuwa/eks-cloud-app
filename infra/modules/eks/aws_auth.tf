0c839f2acc3dd1b26f98a5ba6e83cb8c09d22f4f
Presley Omozuwa <rubblestech@unknownbecccd455392.attlocal.net>
2026-04-20 19:57:01 -0700

    fix four

--- infra/modules/eks/aws_auth.tf
+++ infra/modules/eks/aws_auth.tf
@@ -34,16 +34,12 @@ locals {
 # GITHUB OIDC PROVIDER (HARDENED)
 #############################################
 
-data "tls_certificate" "github" {
-  url = "https://token.actions.githubusercontent.com"
-}
-
 resource "aws_iam_openid_connect_provider" "github" {
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
 
   thumbprint_list = [
-    data.tls_certificate.github.certificates[0].sha1_fingerprint
+     "6938fd4d98bab03faadb97b34396831e3780aea1"
   ]
 }
 
